79d3b126ad61d457d4da6b14f3f4abe070961ee3
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2018-09-24 15:22:42 -0500

    added & modified

---
+++ C++/_integer-single-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variables
+    int num;
+
+    // Ask user to enter a number
+    cout << "Enter the number 2: ";
+
+    // Error Checkin algorithim
+    while (!(cin >> num) || ((num != 2) && (num != 4)))
+    {
+        // Explain error
+        cout << "ERROR: a number must be entered: ";
+        // Clear input stream
+        cin.clear();
+        // Discard previous input
+        cin.ignore(132, '\n');
+    }
+    // Display the number the user has entered
+    cout << "Your favorite number is: " << num << endl;
+    // Salutation
+    cout << "Thanks. Bye." << endl;
+
+    // Terminate program
+    return 0;
+}
